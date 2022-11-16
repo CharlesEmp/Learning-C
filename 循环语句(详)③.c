b76449dd377081c5e9ec3920a93fd3bcdecca2a8
CharlesEmp <Jiang_Yt0927@163.com>
2022-11-16 20:40:51 +0800

    Day 20

---
+++ 循环语句(详)③.c
@@ -0,0 +1,156 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	//if (1)//if 循环
+//	while (1)
+//		printf("hehe\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	while (10 > a)
+//	{
+//		a++;
+//		printf("%d\n", a);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch = getchar();
+//	putchar(ch);
+//	printf("%c\n", ch);
+//	return 0;
+//}
+//int main()
+//{
+//	int ch = 0;
+//	//CTRL+z 
+//	//EOF end of file "-1" 文件结束标志
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ret = 0;
+//	int ch = 0;
+//	char password[20] = { 0 };
+//	printf("请输入密码:>\n");
+//	scanf("%s", password);//输入密码，并存放在password数组中
+//	//缓冲区还剩余一个“\n”
+//	//读取一下“\n”
+//	while ((ch = getchar()) != '\n')
+//	{
+//		;
+//	}
+//	printf("请确认(Y/N):>");
+//	ret = getchar();//Y/N
+//	if (ret == 'y')
+//	{
+//		printf("确认成功\n");
+//	}
+//	else
+//	{
+//		printf("放弃确认\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch > '9')
+//			continue;
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;//初始化
+//	while (i < 10)//判断
+//	{
+//		//......
+//		i++;//调整
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1 ; i <= 10 ; i++)
+//		printf("%d ", i);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if(i==5)
+//			break;
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (i = 5)//i赋值5，并非比较i是否等于5
+//			printf("haha\n");
+//		printf("hehe\n");
+//	}
+//	//死循环
+//	return 0;
+//}
+
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
