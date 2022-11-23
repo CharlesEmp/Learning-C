b31c2c74151ca8ce9d9055d9a91afd9f96ac6911
CharlesEmp <Jiang_Yt0927@163.com>
2022-11-23 21:20:08 +0800

    Day 27

---
+++ 函数②.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = Add(a, b);//输入参数
+//	printf("%d\n", sum);
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	//strlen —— string length —— 与字符串长度有关
+//	//strcpy —— string copy —— 字符串拷贝
+//	char arr1[] = "bit";
+//	char arr2[20] = "##########";
+//	strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+////memset
+////memory(内存) set(设置)
+//int main()
+//{
+//	char arr[] = "Hello World";
+//	memset(arr, '#', 5);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
