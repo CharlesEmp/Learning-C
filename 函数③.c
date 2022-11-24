692303d1bbc024cb10b7006ff25768efa0de581c
CharlesEmp <Jiang_Yt0927@163.com>
2022-11-24 20:14:45 +0800

    Day 28

---
+++ 函数③.c
@@ -0,0 +1,108 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = Add(a, b);//输入参数
+//	printf("%d\n", sum);
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	//strlen —— string length —— 与字符串长度有关
+//	//strcpy —— string copy —— 字符串拷贝
+//	char arr1[] = "bit";
+//	char arr2[20] = "##########";
+//	strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+////memset
+////memory(内存) set(设置)
+//int main()
+//{
+//	char arr[] = "Hello World";
+//	memset(arr, '#', 5);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//定义函数
+//int get_max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	//函数的使用
+//	int max = get_max(a, b);
+//	printf("max = %d\n", max);
+//	return 0;
+//}
+
+////int main()
+////{
+////	int a = 10;
+////	int* pa = &a;//pa指针变量
+////	*pa = 20;//解引用操作
+////	return 0;
+////}
+//// 
+////void Swap1(int x, int y)//error
+////{
+////	int tmp = 0;
+////	tmp = x;
+////	x = y;
+////	y = tmp;
+////}
+// //当实参传给形参的时候
+// //形参其实是实参的一份临时拷贝
+// //对形参的修改是不会改变实参
+//Swap2(int* pa,int* pb)
+//{
+//	int tmp = 0;
+//	tmp = *pa;
+//	*pa = *pb;
+//	*pb = tmp;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	/*int tmp = 0;*/
+//	printf("a = %d , b = %d\n", a, b);
+//
+//	/*tmp = a;
+//	a = b;
+//	b = tmp;*/
+//  //调用Swap1函数（传值调用）
+//	/*Swap1(a, b);*///error
+//  //调用Swap2函数（传址调用）
+//	Swap2(&a, &b);
+//
+//	printf("a = %d , b = %d\n", a, b);
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
